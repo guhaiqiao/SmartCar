21af95dbccb140eefe0e6a149801233a16282369
guhaiqiao <1019931868@qq.com>
2018-09-16 17:01:30 +0800

    update again

--- test_ttl/upper.py
+++ test_ttl/upper.py
@@ -4,7 +4,7 @@ import threading
 from time import ctime, sleep
 import serial
 
-# ----------------------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------------------------
 # message组成： id + command（请求路况信息、收到、发送位置信息、强制减速、路况信息编号）+内容（位置、路况）
 #   id : 0 ~ 9
 #   command:
@@ -13,8 +13,12 @@ import serial
 #       p(position) : 发送位置信息
 #       s(slowdown) : 是否强制减速
 #       t(transfer) : 传输路况信息
-# ----------------------------------------------------------------------------------------------
-message = ['1']
+# -------------------------------------------------------------------------------------------------
+
+message = '0r'
+check = message[0]
+
+
 def send(port, message, time=3):
     if not event.isSet():
         event.set()
@@ -23,8 +27,8 @@ def send(port, message, time=3):
     while count <= 5:
         event.clear()
         # for i in range(1, 11):
-        port.write(id.encode('utf-8'))
-        print("send %s %s" % (id, ctime()))
+        port.write(message.encode('utf-8'))
+        print("send %s %s" % (message, ctime()))
         event.set()
         count += 1
         sleep(time)
@@ -54,20 +58,23 @@ upper = serial.Serial()
 upper.port = 'COM4'
 upper.baudrate = 9600
 upper.timeout = 1
-upper.open()
 
 if __name__ == '__main__':
     alldata = ''   # 接收数据日志
+    upper.open()
     event = threading.Event()
-    send_thread = threading.Thread(target=send, args=(upper, 'ghq'))
+    send_thread = threading.Thread(target=send, args=(upper, message))
+    receive_thread = threading.Thread(target=receive, args=(upper, alldata))
     # 启动发送线程
     send_thread.start()
-    receive_thread = threading.Thread(target=receive, args=(upper, alldata))
     receive_thread.setDaemon(True)
     # 启动接收线程
     receive_thread.start()
+
+    # send_thread.join()
+    # receive_thread.join()
     sleep(20)
     print("total receive: %s" % alldata)
-    print(alldata)
-    upper.close()
+    # print(alldata)
+    # upper.close()
     os.system('pause')
