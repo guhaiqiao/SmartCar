684ef37c086efdafdf46fab9611cea03db8c5dac
guhaiqiao <1019931868@qq.com>
2018-09-21 22:45:36 +0800

    Change Directory Structure

--- gui/showUi.py
+++
@@ -1,9 +0,0 @@
-import pyqt2
-import sys
-from PyQt5 import QtWidgets, uic, QtCore, QtGui
-
-if __name__ == "__main__":
-    app = QtWidgets.QApplication(sys.argv)
-    window = pyqt2.MainUi()
-    window.show()
-    sys.exit(app.exec_())
